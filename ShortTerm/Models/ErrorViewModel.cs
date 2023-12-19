6fc7edf8b970c5ba715baad71ce0209cc099a4ec
Malcolm <machirimalcolm@gmail.com>
2023-12-19 08:33:55 +0200

    Add project files.

---
+++ ShortTerm/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShortTerm.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
